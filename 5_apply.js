2774b29dda6c9632b2761cd90db85bf5b27f02ab
Adarsh Kamble <102070546+Adarsshh34@users.noreply.github.com>
2025-11-06 03:12:36 +0530

    Create 5_apply.js

---
+++ 5_apply.js
@@ -0,0 +1,45 @@
+// 1st Example
+
+let emp1 ={
+    name:"Adarsh",
+    prof:"SE",
+    age:24,
+    getDetails: function(){
+        console.log(this);
+    }
+}
+emp1.getDetails();
+let emp2 ={
+    name:"Sneha",
+    prof:"MBA",
+    age:23
+}
+
+
+emp1.getDetails.call(emp2); // function borrowing
+
+
+// now we can copy paste same getDetail function for all object we can use call here 
+
+// 2nd method 
+// in this we make a common method for all object
+// getDetails is shared among both object using apply
+
+let emp3 ={
+    name:"Adarsh",
+    prof:"SE",
+    age:24,
+    
+}
+function getDetails(){
+    console.log(this);
+}
+
+let emp4 ={
+    name:"Sneha",
+    prof:"MBA",
+    age:23
+}
+
+getDetails.call(emp3); 
+getDetails.call(emp4); 
