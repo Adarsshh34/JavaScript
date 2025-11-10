cf8f6f436395e4df4f7811c9695f82f538545b91
Adarsh Kamble <102070546+Adarsshh34@users.noreply.github.com>
2025-11-11 03:06:34 +0530

    Create 6_Map_reduce_filter_sort_forEach.js

---
+++ 6_Map_reduce_filter_sort_forEach.js
@@ -0,0 +1,45 @@
+const companies =[
+    {name:"Apple",category:"Product base",year:1998},
+    {name:"TCS",category:"Service base", year:1995},
+    {name:"Amazon",category:"Product base", year:2000}
+]
+/*
+for(var i=0;i<companies.length;i++){
+    console.log(companies[i]);
+}
+// ForEach
+companies.forEach((comp)=>{
+    console.log(comp)
+})
+*/
+// Filter - true/false need to be return and entire ans is retured in a
+/*
+const a =companies.filter((comp)=>{
+    return comp.category === "Product base";
+})
+console.log(a);
+*/
+
+// Map
+/*
+const a =companies.map((comp)=>{
+    return comp.name
+})
+console.log(a);
+*/
+
+// Sort
+/*
+const b = companies.sort((comp1,comp2)=>{
+    if(comp1.year > comp2.year) return 1;
+    else return -1;
+});
+console.log(b);
+*/
+
+// Reduce
+let total = 0;
+let c = companies.reduce((total, comp)=>{
+    return  total + comp.year;
+},0);
+console.log(c);
