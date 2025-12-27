bfab315d1e2f3021e45dc929ea8cb19793646e9c
Adarsh Kamble <102070546+Adarsshh34@users.noreply.github.com>
2025-12-28 03:09:58 +0530

    Update 02_Promise.js

--- 02_Promise.js
+++ 02_Promise.js
@@ -38,11 +38,32 @@ let addData = (pname, pprof) =>{
 
 addData("raj","SF",getData).then(getData).catch(err => console.log(err));
 
+1️⃣ Promise.all() -> Resolves when ALL promises resolve, Rejects immediately if ANY promise rejects.
+    const p1 = Promise.resolve(10);
+    const p2 = Promise.resolve(20);
+    const p3 = Promise.resolve(30);
+    
+    Promise.all([p1, p2, p3])
+      .then(result => console.log(result))
+      .catch(err => console.error(err));
 
+2️⃣ Promise.allSettled() -> Resolves when ALL promises get completed (either fullfilled or rejected).
+    const p1 = Promise.resolve(10);
+    const p2 = Promise.reject("Error");
+    const p3 = Promise.resolve(30);
+    
+    Promise.allSettled([p1, p2, p3])
+      .then(results => console.log(results));
 
+3️⃣ Promise.race() -> Resolves when any one of the promise completed(resolve or rejected).
+    const p1 = new Promise(res => setTimeout(res, 2000, "Slow"));
+    const p2 = new Promise(res => setTimeout(res, 1000, "Fast"));
+    
+    Promise.race([p1, p2])
+      .then(result => console.log(result));
+    ANS : Fast
 
-
-
+Promise Chaining : .then().then().then().catch()
 
 
 
