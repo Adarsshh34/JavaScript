9c5871a279ada09ac355a2405bb9ea079d4700b5
Adarsh Kamble <102070546+Adarsshh34@users.noreply.github.com>
2025-12-28 02:45:51 +0530

    Update 01_callback.js

--- 01_callback.js
+++ 01_callback.js
@@ -1,3 +1,24 @@
+Synchronous -> Code runs line by line, and each task blocks the next one until it finishes.
+Asynchronous JS -> Long-running tasks are offloaded, allowing the main thread to continue.
+    
+JS itself is single-threaded, but async behavior is achieved using:
+    Web APIs
+    Event loop
+    Callback queue / Microtask queue
+
+Microtask queue (high priority) -> Microtasks are tasks that are executed immediately after the current call stack is empty and before any macrotask.
+    ✔️  Promise.then()
+    ✔️ Promise.catch()
+    ✔️ Promise.finally()
+    ✔️ queueMicrotask()
+    ✔️ MutationObserver
+Macrotask queue (low priority) -> Macrotasks are tasks scheduled to run after microtasks, usually for timers and events.
+    ✔️ setTimeout
+    ✔️ setInterval
+    ✔️ setImmediate (Node.js)
+    ✔️ DOM events (click, scroll)
+    ✔️ I/O callbacks
+
 A callback function is a function that is passed as an argument to another function 
 and is executed at a later time, typically after some operation completes.
 Example : forEach, setTimeout, map, filter
