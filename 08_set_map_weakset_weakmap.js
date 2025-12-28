382cdce8bd233f2de9139b43e08ee6df29f249f8
Adarsh Kamble <102070546+Adarsshh34@users.noreply.github.com>
2025-12-28 20:05:24 +0530

    Update 08_set_map_weakset_weakmap.js

--- 08_set_map_weakset_weakmap.js
+++ 08_set_map_weakset_weakmap.js
@@ -16,4 +16,31 @@ console.log(map)
 let ws = new WeakSet();
 console.log(ws) // try this in console to see all prototype method
 
-// WeakMap -> we cannot iterate in weakmap
+// WeakMap -> we cannot iterate in weakmap, no size() method
+
+If they can’t be iterated, why were WeakMap and WeakSet created?
+They exist to solve memory-management problems by allowing objects to be garbage-collected automatically, preventing memory leaks.
+Attaching metadata to objects
+  const cache = new Map();
+  
+  function process(obj) {
+    cache.set(obj, "processed");
+  }
+
+Problem ❌
+If obj is no longer used elsewhere
+Map still holds a strong reference
+Object is never garbage collected
+➡️ Memory leak
+
+✅ Solution: WeakMap
+
+const cache = new WeakMap();
+function process(obj) {
+  cache.set(obj, "processed");
+}
+
+Why this works
+WeakMap does not prevent GC
+When obj is gone, its data is removed automatically
+No manual cleanup needed
