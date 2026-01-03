dda8eda35ec775158154f76abf633c83ab82c4d5
Adarsh Kamble <102070546+Adarsshh34@users.noreply.github.com>
2026-01-03 20:20:02 +0530

    Update 002_Function_Scope.js

--- 002_Function_Scope.js
+++ 002_Function_Scope.js
@@ -2,6 +2,7 @@ console.log(add(2, 3)); // Output: 5
 function add(a, b) {
   return a + b;
 }
+// Arrow Function is not hoisted
 // console.log(multiply(2, 3)); // Output: ReferenceError: Cannot access 'multiply' before initialization
 // let multiply = function(a, b) {
 //   return a * b;
