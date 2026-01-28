22a2b6d9bc59555bdf4081ce1adb3a9db1212bad
Adarsh Kamble <102070546+Adarsshh34@users.noreply.github.com>
2026-01-29 03:34:19 +0530

    Update 15_All_About_JWT.js

--- 15_All_About_JWT.js
+++ 15_All_About_JWT.js
@@ -1,3 +1,5 @@
+https://chatgpt.com/c/697a7549-202c-8323-b0c2-72c2e4d1bd41
+
 JWT is a stateless, self-contained token used for authentication and authorization.
   A JWT has 3 parts, separated by .
   HEADER.PAYLOAD.SIGNATURE
@@ -20,4 +22,42 @@ HttpOnly; Secure; SameSite=Strict
 
 what is http Cookies ? -> HttpOnly cookies are normal cookies with an additional security flag that prevents JavaScript access
 
+"withCredentials: true" -> tells the browser to include cookies and authentication credentials in cross-origin HTTP requests. (this flag should be included in every call).
+
+
+2. Handling Token Expiration & Auto Logout
+Problem -> when JWT expire : BE returns 401 : if frontend does nothing -> Bad UX
+
+Option A (Simpler) -> Expire token → auto logout user
+
+Option B (Production-grade) -> access token expires → refresh token generates new one
+------------------------------------------------------------------------------------
+Token	          |  Purpose	             |  Lifetime	     |      Storage          |
+Access Token	  | API authorization	     |   5–15 min	     |   HttpOnly cookie     |
+Refresh Token	  | Get new access token	 |   Days / Weeks	 |   HttpOnly cookie     |
+------------------------------------------------------------------------------------
   
+When Access token expire -> server give 401 -> ui catches it and send request to get new access token using /auth/refresh -> server receive refresh token (validate it)
+-> server send new access token to ui -> now ui uses new access token.
+
+
+3. CORS & Cookie Issues (Very Real 😄)
+When:
+Frontend (React) → http://localhost:3000
+Backend (Spring Boot) → http://localhost:8080
+JWT stored in HttpOnly cookies
+Cookies silently don’t get sent
+Solution : 
+Frontend must send credentials -> axios.post("http://localhost:8080/login", data, { withCredentials: true, });
+Backend must allow credentials -> config.setAllowCredentials(true);
+
+
+4. how to attend authorization
+Question need to see
+
+how to attend authorization like different profile
+admin has different authorization user has different authorization 
+Payload : {"sub": "adarsh","role": "ADMIN","iat": 1700000000,"exp": 1700003600 } 
+how we store role in jwt payload and how we access it
+how we enforces authorization from UI or BE ? which is best way
+like Admin can only access admin panel User cannot access admin panel : so where do we restrict it at ui or be
