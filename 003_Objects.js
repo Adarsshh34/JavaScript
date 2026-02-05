399b8ed76ed035589ca7caf12ce62cf022b8617b
Adarsh Kamble <102070546+Adarsshh34@users.noreply.github.com>
2026-02-06 01:53:05 +0530

    Update 003_Objects.js

--- 003_Objects.js
+++ 003_Objects.js
@@ -1,10 +1,10 @@
 Shallow copy vs deep copy
 
 // 1. Spread operator
-const copy = { ...obj };
+const copy = { ...obj };   [shallow copy]
 
 // 2. Object.assign
-const copy = Object.assign({}, obj);
+const copy = Object.assign({}, obj);  [shallow copy]
 
 // 3. Array shallow copy
 const arrCopy = [...arr];
@@ -21,21 +21,59 @@ const deepCopy = structuredClone(obj);
 ✔️ No data loss
 ✔️ Native & fast
 Limitations
-❌ Cannot copy functions or DOM nodes (usually not needed anyway)
+❌ Cannot copy functions or DOM nodes (usually not needed anyway) or not even for arrow function [DOMException]
 
 
 2. JSON Method 
 const deepCopy = JSON.parse(JSON.stringify(obj));
 ✔️ Object has only plain data
-✔️ No functions, no Dates, no Maps/Sets
+✔️ No functions, no Dates, no Maps/Sets [dates are converted into string, function gets disappear 
 ✔️ No circular references
 Problems 
-❌ Removes functions
+❌ Removes functions 
 ❌ Drops undefined
 ❌ Converts Date to string
 ❌ Breaks on circular references
 
+Example of JSON Methods
+obj -> {
+  '3': 'three',
+  a: 1,
+  b: 'str',
+  arr: [ 1, 2, 3 ],
+  fun: [Function: fun],
+  date: 2026-02-05T20:11:56.410Z,
+  map: Map(1) { 1 => 'adarsh' }
+}
+cloned using JSON method -> {
+  '3': 'three',
+  a: 1,
+  b: 'str',
+  arr: [ 1, 2, 3 ],
+  date: '2026-02-05T20:11:56.410Z',
+  map: {}
+}
+note : circular reference gives error while cloning
 
 Object.freeze() -> Makes an object completely immutable, You cannot Add new properties, Delete existing properties, Modify existing values
-
 Object.seal() -> Prevents adding or deleting properties, Allows modifying existing values
+
+Example :
+var obj ={
+    a:1,
+    b:"str",
+    3:"three",
+    arr:[1,2,3],
+    fun:()=>{
+        console.log("hello");
+    },
+    date: new Date(),
+    map: new Map([[1,"adarsh"]])
+}
+
+Object.freeze(obj);
+Object.seal(obj);
+obj.b = "adarsh";
+obj.name = "kamble";
+console.log(obj);
+
