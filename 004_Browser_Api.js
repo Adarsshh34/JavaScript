3b32da0be53939e1c528f6a3bd4bad53b4fe0cbb
Adarsh Kamble <102070546+Adarsshh34@users.noreply.github.com>
2026-02-06 02:44:48 +0530

    Update 004_Browser_Api.js

--- 004_Browser_Api.js
+++ 004_Browser_Api.js
@@ -37,12 +37,46 @@ parent.removeChild(child) -> old method, precise, work for all browser
 Event bubbling, capturing, delegation
 Event Bubbling (Default behavior) -> Event starts from the target element and moves up to parent → document (child to parent)
 Event Capturing (Trickling) -> Event starts from document → parent → child (Capturing phase is disabled by default) (parent to child)
-Event Delegation -> Event delegation is a technique where you attach a single event listener to a parent element instead of multiple child elements, and handle child events using event bubbling.
 
 To stop bubbling -> e.stopPropagation();
 To Stop bubbling + capturing completely -> e.stopImmediatePropagation();
 e.preventDefault() -> stops the browser’s default behavior associated with an event.
-  
+
+Event Delegation -> Event delegation is a technique where you attach a single event listener to a parent element instead of multiple child elements, and handle child events using event bubbling.
+approach 1: using data-action
+<div id="container">
+  <button class="btn" data-action="add">Add</button>
+  <button class="btn" data-action="edit">Edit</button>
+  <button class="btn" data-action="delete">Delete</button>
+</div>
+const container = document.getElementById("container");
+function handleAdd=()=> console.log("Add clicked"); 
+function handleEdit()=> console.log("Edit clicked"); 
+function handleDelete()=> console.log("Delete clicked"); 
+container.addEventListener("click", (e) => {
+  const btn = e.target.closest(".btn");
+  if (!btn) return;
+  const action = btn.dataset.action;
+  if (action === "add") handleAdd();
+  if (action === "edit") handleEdit();
+  if (action === "delete") handleDelete();
+});
+
+approach 2: using map for actions
+const actions = {
+  add: () => console.log("Add clicked"),
+  edit: () => console.log("Edit clicked"),
+  delete: () => console.log("Delete clicked")
+};
+container.addEventListener("click", (e) => {
+  const btn = e.target.closest(".btn");
+  if (!btn) return;
+  actions[btn.dataset.action]?.();
+});
+
+Approach 3: Based on text (NOT recommended)
+❌ Breaks if text changes
+❌ Bad for i18n [bad for when app is used for different lang then text would not match]
 
 LocalStorage vs SessionStorage vs Cookies
 LocalStorage -> stores data permanently in the browser (until manually cleared).
