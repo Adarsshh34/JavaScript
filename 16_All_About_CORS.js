668fd83eb44260e0c552017bcaa2add1c95a7522
Adarsh Kamble <102070546+Adarsshh34@users.noreply.github.com>
2026-02-10 19:28:41 +0530

    Create 16_All_About_CORS.js

---
+++ 16_All_About_CORS.js
@@ -0,0 +1,8 @@
+CORS -> The browser blocks it, NOT the server.
+How CORS Blocking Actually Works (Step-by-Step)
+🔹 Case 1: Simple Cross-Origin Request (GET)
+1️⃣ Frontend (http://siteA.com) makes request to backend (http://siteB.com)
+2️⃣ Browser sends request to server
+3️⃣ Server responds (200 OK)
+4️⃣ Browser checks response headers:
+
